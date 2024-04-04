a9ddc74f3f47380d9f20c45f783be507f26ee78b
Bashu-011 <bashu2609@gmail.com>
2024-04-04 04:43:59 +0300

    Resubmission

--- server/app.py
+++ server/app.py
@@ -20,6 +20,15 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake = db.session.get(Earthquake, id)
+    if earthquake is None:
+        return make_response({"message": f"Earthquake {id} not found."}, 404)
+
+    return make_response(earthquake.to_dict(), 200)
+
+
 # Add views here
 @app.route('/earthquakes/magnitude/<float:magnitude>')
 def power(magnitude):
